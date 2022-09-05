04e7fdf79f83f08c54c9adfb4f660f70da8d0938
Suraj Kumar <surajkumargautam7889@gmail.com>
2022-09-05 23:54:23 +0530

    table add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const users = [
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+    { id: 1, name: "Suraj", email: "Suraj@gmail.com", contact: 123456 },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-expand navbar-light bg-light">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <span>Home</span>
+          </li>
+        </ul>
+      </nav>
+      <div className="container mt-2">
+        <table class="table table-hover table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Contact</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.contact}</td>
+                  <td>
+                    <a className='btn btn-success' >Views</a>&nbsp;
+                    <a className='btn btn-primary'>Edit</a>&nbsp;
+                    <a className='btn btn-danger' >Delete</a></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
